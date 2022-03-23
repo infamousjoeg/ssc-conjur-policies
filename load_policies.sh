e8f3ffa11890bf262b413969b7fa07c557984e90
Joe Garcia <joe@joe-garcia.com>
2022-03-23 19:17:33 -0400

    Finalize automated load

--- load_policies.sh
+++ load_policies.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-SESSION_TOKEN=""
+export SESSION_TOKEN=""
 
 main () {
-  SESSION_TOKEN=$(authenticate)
+  SESSION_TOKEN=$(authenticate admin)
   # Begin loading policies
   load_policy root root.yml
+  load_policy root users.yml
+  load_policy root groups.yml
+  load_policy root grants/grants_users.yml
+  load_policy ci ci/jenkins/jenkins.yml
+  load_policy ci/jenkins ci/jenkins/conjur-policies.yml
+  load_policy root authn/authn-jwt-jenkins.yml
+  load_policy root grants/grants_authn.yml
 }
 
-def authenticate () {
-  curl -k -s --header "Accept-Encoding: base64" -X POST --data "$CONJUR_API_KEY" https://conjur.cybr.rocks/authn/cyberarkdemo/admin/authenticate
+authenticate () {
+  curl -k -s --header "Accept-Encoding: base64" -X POST --data "$CONJUR_API_KEY" https://conjur.cybr.rocks/authn/cyberarkdemo/"$1"/authenticate
 }
 
-def load_policy () {
-  curl -k -s --header "Authorization: Token token=\"$SESSION_TOKEN\"" -X POST -d "$(cat $2)" https://conjur.cybr.rocks/policies/cyberarkdemo/policy/"$1" > /dev/null
+load_policy () {
+  urlencoded_policy=$(echo -n $1 | sed 's/\//\%2F/g')
+  curl -k -s --header "Authorization: Token token=\"$SESSION_TOKEN\"" -X POST -d "$(cat "$2")" https://conjur.cybr.rocks/policies/cyberarkdemo/policy/"$urlencoded_policy"
 }
 
 main "$@"
