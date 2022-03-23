dd7a4dde743cc4ce11ffa1b6940d7c8003b3edeb
Joe Garcia <joe@joe-garcia.com>
2022-03-23 19:29:30 -0400

    Add grants_hosts.yml load

--- load_policies.sh
+++ load_policies.sh
@@ -3,7 +3,7 @@
 export SESSION_TOKEN=""
 
 main () {
-  SESSION_TOKEN=$(authenticate admin)
+  SESSION_TOKEN=$(authenticate host%2Fci%2Fjenkins%2Fconjur-policies)
   # Begin loading policies
   load_policy root root.yml
   load_policy root users.yml
@@ -13,6 +13,7 @@ main () {
   load_policy ci/jenkins ci/jenkins/conjur-policies.yml
   load_policy root authn/authn-jwt-jenkins.yml
   load_policy root grants/grants_authn.yml
+  load_policy root grants/grants_hosts.yml
 }
 
 authenticate () {
@@ -20,7 +21,7 @@ authenticate () {
 }
 
 load_policy () {
-  urlencoded_policy=$(echo -n $1 | sed 's/\//\%2F/g')
+  urlencoded_policy=$(echo -n "$1" | sed 's/\//\%2F/g')
   curl -k -s --header "Authorization: Token token=\"$SESSION_TOKEN\"" -X POST -d "$(cat "$2")" https://conjur.cybr.rocks/policies/cyberarkdemo/policy/"$urlencoded_policy"
 }
 
