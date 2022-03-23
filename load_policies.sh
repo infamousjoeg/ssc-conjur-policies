e9e3d1264009b5e3e7d348510c6f82409dd252b0
Joe Garcia <infamousjoeg@users.noreply.github.com>
2022-03-23 17:42:45 -0400

    Update load_policies.sh

--- load_policies.sh
+++ load_policies.sh
@@ -9,11 +9,11 @@ main () {
 }
 
 def authenticate () {
-  curl -k -s --header "Accept-Encoding: base64" -X POST --data "$CONJUR_API_KEY" https://localhost/authn/cyberarkdemo/admin/authenticate
+  curl -k -s --header "Accept-Encoding: base64" -X POST --data "$CONJUR_API_KEY" https://conjur.cybr.rocks/authn/cyberarkdemo/admin/authenticate
 }
 
 def load_policy () {
-  curl -k -s --header "Authorization: Token token=\"$SESSION_TOKEN\"" -X POST -d "$(cat $2)" https://localhost/policies/cyberarkdemo/policy/"$1" > /dev/null
+  curl -k -s --header "Authorization: Token token=\"$SESSION_TOKEN\"" -X POST -d "$(cat $2)" https://conjur.cybr.rocks/policies/cyberarkdemo/policy/"$1" > /dev/null
 }
 
 main "$@"
